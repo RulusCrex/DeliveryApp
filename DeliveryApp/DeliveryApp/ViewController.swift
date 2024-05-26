ec77cebb3b19101269971ac2d250176a548981e9
Pave <RulusCrex>
2024-05-26 15:39:05 +0500

    Init main/ pod instal

---
+++ DeliveryApp/DeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by Павел Дмитриевич on 26.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
